3342692f3ae50284e5590595c30d5ec5d00a801b
LIWKaguya <hungle120901@gmail.com>
2021-12-24 00:08:09 +0200

    Exercise 9.2

---
+++ first/exerciseCalculator.ts
@@ -0,0 +1,53 @@
+interface exerciseCal {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+interface ExerValues {
+    target: number,
+    exerciseHours: Array<number>
+}
+
+const parseArguments = (args: Array<string>): ExerValues => {
+    if (args.length < 2) throw new Error('Not enough arguments');
+    const exerciseHours = args.slice(3, args.length).map(n => Number(n))
+    const target = Number(args[2])
+    return {target, exerciseHours}    
+}
+
+const exerciseCal = ( target: number, exerciseHours: Array<number>): exerciseCal => {
+    const periodLength = exerciseHours.length
+    const trainingDays = exerciseHours.filter(n => n>0).length
+    const average = (exerciseHours.reduce((a, b) => (a+b)))/periodLength
+    const success = average >= target
+    let rating, ratingDescription
+    if(average < target) {
+        rating = 1
+        ratingDescription = "not too bad but could be better"
+    }
+    else if (average === target) {
+        rating = 2 
+        ratingDescription = "ok"
+    }
+    else {
+        rating = 3
+        ratingDescription = "good"
+    }
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+const {target, exerciseHours} = parseArguments(process.argv)
+console.log(exerciseCal(target, exerciseHours))
